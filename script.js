cf1430657e8fca1da30c3240a0e74af68d94581e
Bianca <130195861+bianca-8@users.noreply.github.com>
2024-08-16 09:47:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,59 @@ function filter() {
     var input, filter, items, value;
 
     input = document.getElementById('search');
-    filter = input.value.toUpperCase();
+    filter = input.value.toLowerCase();
     items = document.getElementsByClassName('item');
 
     for (i = 0; i < items.length; i++) {
         value = items[i].textContent || items[i].innerText;
         
-        if (value.toUpperCase().indexOf(filter) > -1) {
+        if (value.toLowerCase().indexOf(filter) > -1) {
             items[i].style.display = "";
         } 
         else {
             items[i].style.display = "none";
         }
+    }
+}
+
+function filterTitle() {
+    var input, filter, items, titles, value;
+
+    input = document.getElementById('search');
+    filter = input.value.toLowerCase().trim();
+    items = document.getElementsByClassName('item');
+    titles = document.getElementsByClassName('title');
+
+    for (var i = 0; i < titles.length; i++) {
+        value = titles[i].textContent || titles[i].innerText;
+        value = value.toLowerCase().trim();
+        
+        if (filter === "" || value.startsWith(filter)) {
+            items[i].style.display = "";
+        } 
+        else {
+            items[i].style.display = "none";
+        }
+    }
+}
+
+var checkbox = false;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('filterTitle').addEventListener("change", () => {
+        if ( checkbox.checked ) {
+            checkbox = false;
+        } else {
+            checkbox = true;
+        }
+    });
+});
+
+function newFilter() {
+    if (!checkbox) {
+        filter()
+    }
+    else {
+        filterTitle()
     }
 }
 
